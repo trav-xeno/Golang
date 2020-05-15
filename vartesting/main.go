349ee6ca3bd50d54b050e7819d7fa1c50bdabb60
trav-xeno <travisnevins@outlook.com>
2020-05-15 16:45:00 -0400

    added random string because why not

--- vartesting/main.go
+++ vartesting/main.go
@@ -7,8 +7,12 @@ import (
 )
 
 func main() {
+	//set unqiue seed
+	rand.Seed(time.Now().UnixNano())
 	fmt.Println("current time is ", time.Now())
 	printRandom()
+	randomMain()
+
 	fmt.Println("call hello funciont")
 	str := hello()
 	fmt.Println(str)
@@ -16,13 +20,31 @@ func main() {
 	foobar(16)
 }
 
+// Returns an int >= min, < max
+func randomInt(min, max int) int {
+	return min + rand.Intn(max-min)
+}
+
+func randomMain() {
+	fmt.Println("random between 5 and 50")
+	fmt.Println(randomInt(5, 50)) //get an int in the 1...10 range
+	fmt.Println("random string: ", randomString(20))
+}
+
 func printRandom() {
-	rand.Seed(time.Now().UnixNano())
 	fmt.Println("random number printed seed based on time")
 	fmt.Println(rand.Intn(30))
 	fmt.Println(rand.Intn(30))
 	fmt.Println(rand.Intn(30))
 	fmt.Println("--------------------")
+}
+
+func randomString(len int) string {
+	bytes := make([]byte, len)
+	for i := 0; i < len; i++ {
+		bytes[i] = byte(randomInt(65, 90))
+	}
+	return string(bytes)
 }
 
 // foobar divided by 3 foo and divided by 5 bar both foobar else print number
--- vartesting/readme.md
+++ vartesting/readme.md
@@ -1,6 +1,6 @@
 # Go var and function testing
 this is jsut a simple app that show the different basic types Go has. 
-This app will print random numbers based on seeded current time
+This app will print random numbers and random numbers between a range. The seed is the current time
 This will run a the classic foo bar interview question funciotn.
 The foobar quesion is given an number n:
 * print foo if the nubmer is divide by 3
