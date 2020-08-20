ec296f213b7f17794770ee4968198335c3a2baa4
“trav-xeno” <travisnevins@outlook.com>
2020-08-19 20:19:57 -0400

    updated comments and index.html

--- goWeb/Templates/main.go
+++ goWeb/Templates/main.go
@@ -23,9 +23,11 @@ func main() {
 	//tpl is template pointer with returned read files data
 	tpl, err := template.ParseFiles("index.gohtml")
 	var name, food, fact string
+	//print questions to user
 	fmt.Println("Please enter your first name: ")
-	fmt.Scan(&name)
+	fmt.Scan(&name) //single word input only but I hsould add a check to make sure they only enter one word
 	fmt.Println("Please enter your favorite food: ")
+	// take multiple words input
 	foodReader := bufio.NewReader(os.Stdin) //create new reader
 	food, readErr := foodReader.ReadString('\n')
 	if readErr != nil {
@@ -42,6 +44,7 @@ func main() {
 
 	if err != nil {
 		log.Fatalln(err)
+		fmt.Println("an error occured while read input!")
 	}
 	//this part reads template and prints to commandline and takes in data to add to webpage
 	// or
@@ -55,11 +58,13 @@ func main() {
 		//still needs template becuase it basially is a pointer to collection oif templates
 	    err = tpl.Execure(nf, nil) //take in no data
 	*/
+	//create user object based on input
 	user := person{Name: name, FavFood: food, FunFact: fact}
-	fmt.Println("name: " + user.Name + "\n favorite food: " + user.FavFood + "\n fun fact: " + user.FunFact)
+	//fmt.Println("name: " + user.Name + "\n favorite food: " + user.FavFood + "\n fun fact: " + user.FunFact)
 	err = tpl.Execute(os.Stdout, user)
 	if err != nil {
 		log.Fatalln(err)
+		fmt.Println("There was an err excecuting the template.")
 	}
 	f, err := os.Create("./index.html")
 	if err != nil {
